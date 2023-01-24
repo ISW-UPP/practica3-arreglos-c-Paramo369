2cd1c782a64dd93b52883d5e392bda1d13eeb6de
Fernando Paramo <111932657+Paramo369@users.noreply.github.com>
2023-01-24 17:56:19 -0600

    Update Practica3Arreglos.cpp

--- Practica3Arreglos.cpp
+++ Practica3Arreglos.cpp
@@ -1,4 +1,4 @@
-// Practica3Arreglos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// Practica3_Arreglos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
 #include <iostream>
@@ -7,6 +7,7 @@ using namespace std;
 
 int main()
 {
+
     //Programa que pida 10 números y los muestre en orden inverso al que fueron agregados.
     int i;
     int numeros[10];
@@ -29,24 +30,34 @@ int main()
     //Programa que pida 10 números, calcule su media y luego muestre los que están por encima de la media.
     int x;
     int numeros2[10];
-    float media = 0;
+    float media = 0, suma = 0;
 
     for (x = 0; x < 10; x++)
     {
         cout << "Ingresa el numero " << x + 1 << ": ";
         cin >> numeros2[x];
     }
-    
+
     cout << "\n";
 
     for (x = 0; x < 10; x++)
     {
-        media += numeros2[x];
+        suma += numeros2[x];
     }
 
-    cout << "La media es: " << media / 10 << endl;
+    media = suma / 10;
+    cout << "La media es: " << media << endl;
 
-    cout << "----------------------------------------------------------" << endl;
+    cout << "\n";
+
+    for (x = 0; x < 10; x++)
+    {
+        if (numeros2[x] > media) {
+            cout << numeros2[x] << endl;
+        }
+    }
+
+    cout << "\n----------------------------------------------------------" << endl;
 
     //Programa que pida 10 números, para que calcule cuál es el mayor de ellos.
     int z;
@@ -61,9 +72,9 @@ int main()
 
     for (z = 0; z <= 10; z++)
     {
-        if (z > mayor)
+        if (numeros3[z] > mayor)
         {
-            mayor = z;
+            mayor = numeros3[z];
         }
     }
 
